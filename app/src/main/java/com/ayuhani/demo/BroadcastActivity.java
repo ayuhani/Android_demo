3370da3265fc7b59d25c180f5f2b152b73dc9750
ayuhani <1132529645@qq.com>
2018-05-02 18:16:30 +0800

    broadcast receiver demo

---
+++ app/src/main/java/com/ayuhani/demo/BroadcastActivity.java
@@ -0,0 +1,55 @@
+package com.ayuhani.demo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class BroadcastActivity extends AppCompatActivity {
+
+    private IntentFilter intentFilter;
+    private LocalReceiver localReceiver;
+    private LocalBroadcastManager localBroadcastManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_broadcast);
+
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
+
+        Button button = findViewById(R.id.btn_send);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent("com.ayuhani.broadcast.LOCAL_BROADCAST");
+                localBroadcastManager.sendBroadcast(it);
+            }
+        });
+
+        intentFilter = new IntentFilter("com.ayuhani.broadcast.LOCAL_BROADCAST");
+        localReceiver = new LocalReceiver();
+        localBroadcastManager.registerReceiver(localReceiver, intentFilter);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        localBroadcastManager.unregisterReceiver(localReceiver);
+    }
+
+    class LocalReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "received local broadcast", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
