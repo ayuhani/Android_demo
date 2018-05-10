c99962f1b88e0658c79ec0e0e8bd86c388342abd
ayuhani <1132529645@qq.com>
2018-05-10 11:26:16 +0800

    contentprovider demo

---
+++ app/src/main/java/com/ayuhani/demo/contentprovider/ProviderActivity.java
@@ -0,0 +1,13 @@
+package com.ayuhani.demo.contentprovider;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProviderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_provider);
+    }
+}
