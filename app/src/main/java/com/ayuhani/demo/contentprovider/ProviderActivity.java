5e94bdafc6c43a14d789dc4c6edecc3acd37302d
ayuhani <1132529645@qq.com>
2018-05-10 11:38:15 +0800

    content provider demo

--- app/src/main/java/com/ayuhani/demo/contentprovider/ProviderActivity.java
+++ app/src/main/java/com/ayuhani/demo/contentprovider/ProviderActivity.java
@@ -1,13 +1,86 @@
 package com.ayuhani.demo.contentprovider;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.ayuhani.demo.R;
 
 public class ProviderActivity extends AppCompatActivity {
+
+    private String newId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provider);
+
+        Button btnAdd = findViewById(R.id.btn_add_data);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 添加数据
+                Uri uri = Uri.parse("content://com.ayuhani.demo.provider/book");
+                ContentValues values = new ContentValues();
+                values.put("name", "A Clash of Kings");
+                values.put("author", "George Martin");
+                values.put("pages", 1040);
+                values.put("price", 22.85);
+                Uri newUri = getContentResolver().insert(uri, values);
+                newId = newUri.getPathSegments().get(1);
+            }
+        });
+
+        Button btnQuery = findViewById(R.id.btn_query_data);
+        btnQuery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 查询数据
+                Uri uri = Uri.parse("content://com.ayuhani.demo.provider/book");
+                Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+                if (cursor != null) {
+                    while (cursor.moveToNext()) {
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+                        Log.d("ProviderActivity", "book name is " + name);
+                        Log.d("ProviderActivity", "book author is " + author);
+                        Log.d("ProviderActivity", "book pages is " + pages);
+                        Log.d("ProviderActivity", "book price is " + price);
+                    }
+                    cursor.close();
+                }
+            }
+        });
+
+        Button btnUpdate = findViewById(R.id.btn_update_data);
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 更新数据
+                Uri uri = Uri.parse("content://com.ayuhani.demo.provider/book/" + newId);
+                ContentValues values = new ContentValues();
+                values.put("name", "A Storm of Swords");
+                values.put("pages", 1216);
+                values.put("price", 24.05);
+                getContentResolver().update(uri, values, null, null);
+            }
+        });
+
+        Button btnDel = findViewById(R.id.btn_del_data);
+        btnDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 删除数据
+                Uri uri = Uri.parse("content://com.ayuhani.demo.provider/book/" + newId);
+                getContentResolver().delete(uri, null, null);
+            }
+        });
     }
 }
