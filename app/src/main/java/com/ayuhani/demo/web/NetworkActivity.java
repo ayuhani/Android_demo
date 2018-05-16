c2c0d0775da8938551cc1b4b0f23781c1b57b66f
ayuhani <1132529645@qq.com>
2018-05-16 17:18:03 +0800

    JSONObject demo

--- app/src/main/java/com/ayuhani/demo/web/NetworkActivity.java
+++ app/src/main/java/com/ayuhani/demo/web/NetworkActivity.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 
 import com.ayuhani.demo.R;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.xml.sax.InputSource;
 import org.xml.sax.XMLReader;
 import org.xmlpull.v1.XmlPullParser;
@@ -58,16 +60,34 @@ public class NetworkActivity extends AppCompatActivity implements View.OnClickLi
             public void run() {
                 try {
                     OkHttpClient client = new OkHttpClient();
-                    Request request = new Request.Builder().url("http://192.168.0.101/get_data.xml").build();
+                    Request request = new Request.Builder().url("http://192.168.0.101/get_data.json").build();
                     Response response = client.newCall(request).execute();
                     String responseData = response.body().string();
 //                    parseXMLWithPull(responseData);
-                    parseXMLWithSax(responseData);
+//                    parseXMLWithSax(responseData);
+                    parseJSONWithJSONObject(responseData);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }).start();
+    }
+
+    private void parseJSONWithJSONObject(String jsonData) {
+        try {
+            JSONArray array = new JSONArray(jsonData);
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject object = array.getJSONObject(i);
+                String id = object.getString("id");
+                String name = object.getString("name");
+                String version = object.getString("version");
+                Log.d("NetworkActivity", "id is " + id);
+                Log.d("NetworkActivity", "name is " + name);
+                Log.d("NetworkActivity", "version is " + version);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     private void parseXMLWithSax(String xmlData) {
